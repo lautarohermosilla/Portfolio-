b7318f59f6bca6ebc0da0df85e5ff774f133be0c
Lautaro Hermosilla <hermosillautaro@gmail.com>
2022-09-08 20:26:53 -0300

    creando portfolio!

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const header = document.querySelector ("header");
+
+/*----sticky navegador-----*/
+
+function stickyNavbar(){
+ header.classList.toggle("scrolled", window.pageYOffset > 0);
+   
+}
+
+stickyNavbar();
+
+window.addEventListener ("scroll", stickyNavbar);
+
+/*-----scroll revead-*/
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px"
+})
+
+sr.reveal(".showcase-info", {delay: 600});
+sr.reveal(".showcase-image", {origin:top,delay: 700});
